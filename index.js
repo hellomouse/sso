76594effb6e2dad948866a03ffa2d568853bc4b1
hellomouse <hellomouse@hellomouse.net>
2019-01-20 03:37:17 +0200

    update sso to have logout page. also do stuff with login page

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const htpasswd = require('htpasswd-js');
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const redis = require("redis");
+const crypto = require("crypto");
+
 var client = redis.createClient();
 
 client.on('error', function (err) {
@@ -17,6 +20,7 @@ const htpasswd_loc = process.env.HTPASSWD || '/opt/tengine/conf/passwd';
 
 /* actual login request */
 app.use(bodyParser.json());
+app.use(cookieParser());
 app.post('/auth', function (req, res) {
   htpasswd.authenticate({
     username: req.body.user,
@@ -24,10 +28,10 @@ app.post('/auth', function (req, res) {
     file: htpasswd_loc
   }).then((result) => {
     if (result) {
-      const my_key = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-      /* 604800000 miliseconds = 1 week */
-      client.set(my_key, 'potato', 'EX', 604800000);
-      res.cookie('ra_cookie', my_key, {expires: new Date(Date.now() + 604800000), secure: true, domain: domain});
+      const my_key = crypto.randomBytes(16).toString("base64").replace(/[=/+]/g,"");
+      /* 6048000 seconds = 10 weeks */
+      client.set(my_key, 'potato', 'EX', 6048000);
+      res.cookie('ra_cookie', my_key, {secure: true, domain: domain});
       res.end('{"ok":true}');
     } else {
       res.end('{"ok":false}');
@@ -35,15 +39,23 @@ app.post('/auth', function (req, res) {
   });
 });
 
+app.get('/logout', function (req, res) {
+  if (req.cookies && req.cookies['ra_cookie']) {
+    const my_key =  req.cookies['ra_cookie'];
+    client.exists(my_key, function (err, reply) {
+      if (err) res.end('{"ok":false}');
+      if (reply != 1) res.end('{"ok":false}');
+      client.del(my_key);
+      res.clearCookie('ra_cookie');
+      res.end('{"ok":true}');
+    });
+  } else {
+    res.end('{"ok":false}');
+  }
+});
 
 /* send static assets */
 app.use('/', express.static('static'));
-
-app.get('*', function(req, res) {
-  console.log(req);
-  console.log(res);
-  res.end('');
-});
 
 app.listen(port, function () {
     console.log(`SSO Authentication Server running on 0.0.0.0:${port}`);
