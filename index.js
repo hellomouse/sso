e6078806b7d47ce0529653353dacb0201e29133c
ohnx <me@masonx.ca>
2019-01-21 06:04:03 +0200

    use postgresql instead of redis
    ok fine @iczero

--- index.js
+++ index.js
@@ -1,71 +1,57 @@
-const htpasswd = require('htpasswd-js');
+const argon2 = require('argon2');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const redis = require("redis");
 const crypto = require("crypto");
 const otplib = require("otplib");
-const bluebird = require("bluebird");
-bluebird.promisifyAll(redis);
-
-var client = redis.createClient();
-
-client.on('error', function (err) {
-    console.log(`redis encountered error: ${err}`);
+const Client = require('pg').Client
+const client = new Client({
+  user: 'hellomouse',
+  host: '/var/run/postgresql',
+  database: 'sso',
+  password: null,
+  port: 5432
 });
+client.connect();
 
 const app = express();
 
 /* config */
 const port = process.env.PORT || 2971;
 const domain = process.env.DOMAIN || 'hellomouse.net';
-const htpasswd_loc = process.env.HTPASSWD || '/opt/tengine/conf/passwd';
 
 /* actual login request */
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.post('/auth', function (req, res) {
-  htpasswd.authenticate({
-    username: req.body.user,
-    password: req.body.password,
-    file: htpasswd_loc
-  }).then((result) => {
-    if (result) {
-      client.getAsync(`${req.body.user}-2fa`).then(secret => {
-        if(secret) {
-          if(req.body.totp != otplib.authenticator.generate(secret)) return res.end('{"ok":false}');
-        }
-        const my_key = crypto.randomBytes(16).toString("base64").replace(/[=/+]/g,"");
-        /* 6048000 seconds = 10 weeks */
-        client.set(my_key, req.body.user, 'EX', 6048000);
-        res.cookie('ra_cookie', my_key, {secure: true, domain: domain});
-        res.end('{"ok":true}');
-      });
-    } else {
-      res.end('{"ok":false}');
-    }
+  client.query('SELECT * FROM logins WHERE username = $1', [req.body.user], (error, results) => {
+    if(error) return res.end('{"ok":false}');
+    if(results.rows.length != 1) return res.end('{"ok":false}');
+    const user = results.rows[0];
+    argon2.verify(user.password, req.body.password).then(match =>{
+      if(!match) return res.end('{"ok":false}');
+      if(user.totp_token && otplib.authenticator.generate(user.totp_token) != req.body.totp) return res.end('{"ok":false}');
+      const key = crypto.randomBytes(32).toString("base64").replace(/[=/+]/g,"");
+      client.query('INSERT INTO sessions (session_key, username, expiry) VALUES ($1, $2, $3)', [key, req.body.user, new Date(Date.now() + 604800000)]);
+      res.cookie('ra_cookie', key, {secure: true, domain: domain});
+      res.end('{"ok": true}');
+    });
   });
 });
 
 app.get('/logout', function (req, res) {
   if (req.cookies && req.cookies['ra_cookie']) {
-    const my_key =  req.cookies['ra_cookie'];
-    client.exists(my_key, function (err, reply) {
-      if (err) res.end('{"ok":false}');
-      if (reply != 1) res.end('{"ok":false}');
-      client.del(my_key);
+    const key =  req.cookies['ra_cookie'];
+      client.query('DELETE FROM sessions WHERE session_key = $1', [key]);
       res.cookie('ra_cookie', '', {expires: new Date(), secure: true, domain: domain});
       //res.send('{"ok":true}');
-      res.redirect("/");
-    });
-  } else {
-    res.end('{"ok":false}');
+      res.redirect("/?"+Math.random());
   }
 });
 app.get("/2fa", (req, res) => {
-  client.existsAsync(`${req.body.user}-2fa`).then(reply => {
-    if(reply != 1) res.end("false");
-      else res.end("true");
+  client.query('SELECT * FROM logins WHERE username = $1', [req.body.user], (error, results) => {
+    if(results.rows[0].totp_token) res.send('true');
+      else res.send('false');
   });
 });
 /* send static assets */
